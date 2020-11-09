f1d5fbfe06fd5bfe3342ac1c82594f3c8cbf2495
JonathanW82 <Jonwheway@live.co.uk>
2020-11-09 15:20:57 +0000

    added functionality to get absolute file paths

--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+import os
+from xhtml2pdf import pisa
+from .models import Customer
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
 from django.views.generic import ListView
-from .models import Customer
+from django.conf import settings
+from django.contrib.staticfiles import finders
 
 
 class CustomerListView(ListView):
@@ -29,7 +32,7 @@ def customer_render_pdf_view(request, *args, **kwargs):
 
     # create a pdf
     pisa_status = pisa.CreatePDF(
-       html, dest=response)
+       html, dest=response, link_callback=link_callback)
     # if error then show some funy view
     if pisa_status.err:
         return HttpResponse('We had some errors <pre>' + html + '</pre>')
@@ -51,8 +54,40 @@ def render_pdf_view(request):
 
     # create a pdf
     pisa_status = pisa.CreatePDF(
-       html, dest=response)
+       html, dest=response, link_callback=link_callback)
     # if error then show some funy view
     if pisa_status.err:
         return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
+
+
+def link_callback(uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        result = finders.find(uri)
+        if result:
+            if not isinstance(result, (list, tuple)):
+                result = [result]
+            result = list(os.path.realpath(path) for path in result)
+            path = result[0]
+        else:
+            sUrl = settings.STATIC_URL        # Typically /static/
+            sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
+            mUrl = settings.MEDIA_URL         # Typically /media/
+            mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
+
+            if uri.startswith(mUrl):
+                path = os.path.join(mRoot, uri.replace(mUrl, ""))
+            elif uri.startswith(sUrl):
+                path = os.path.join(sRoot, uri.replace(sUrl, ""))
+            else:
+                return uri
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+            raise Exception(
+                    'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+        return path
